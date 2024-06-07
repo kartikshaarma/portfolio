d61ada87a7399801a2284cf46202fb93fe7e8031
kartikshaarma <155128412+kartikshaarma@users.noreply.github.com>
2024-06-07 16:36:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+
+    // Hover effects
+    $('.card').hover(
+        function() {
+            $(this).addClass('shadow-lg');
+        }, function() {
+            $(this).removeClass('shadow-lg');
+        }
+    );
+});
